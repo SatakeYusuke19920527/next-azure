aa2b5dd5530d20c7cd095a99f72a4db61e582eae
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-08-14 15:06:11 +0900

    commit

--- lib/get-products.ts
+++ lib/get-products.ts
@@ -1,10 +1,10 @@
 
-const url = process.env.NEXT_PUBLIC_STRIPE_PRODUCT_URL!
 /**
  * stripeの商品一覧を取得する
  * @returns products
  */
 export async function getProducts() {
+  const url = process.env.NEXT_PUBLIC_STRIPE_PRODUCT_URL!
   const res = await fetch(`${url}/api/fetch_products`);
   const products = await res.json();
   return {
