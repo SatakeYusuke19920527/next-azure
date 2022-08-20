96bdaa8136a18669586d9ab32dff728a5efdd087
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-08-20 14:54:25 +0900

    url 変更

--- lib/get-products.ts
+++ lib/get-products.ts
@@ -4,7 +4,7 @@
  */
 export async function getProducts() {
   const url = process.env.NEXT_PUBLIC_STRIPE_PRODUCT_URL!
-  const res = await fetch(`http://localhost:3000/api/fetch_products`);
+  const res = await fetch(`https://green-pond-08be32800.1.azurestaticapps.net/api/fetch_products`);
   const products = await res.json();
   return {
     props: {
